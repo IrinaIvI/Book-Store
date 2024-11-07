bb338e5b3257036a831b3098299277a398560d32
Ira <ira.ivashko.99@gmail.com>
2024-11-07 21:16:26 +0300

    Создание моделей

--- BookStore/book_store/models.py
+++ BookStore/book_store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+
+class Book(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=20)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    count = models.IntegerField(default=0)
