25636d01b388eb0f139d61a2dd5a27cb0c82c62b
Ira <ira.ivashko.99@gmail.com>
2024-11-07 22:33:31 +0300

    Добавлено строковое представление для моделей

--- BookStore/book_store/models.py
+++ BookStore/book_store/models.py
@@ -5,8 +5,14 @@ class Author(models.Model):
     first_name = models.CharField(max_length=10)
     last_name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Book(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=20)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
     count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
