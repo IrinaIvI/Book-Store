0e26ac2d9b56be2e4fd6c8aa9c4a5795da309952
Ira <ira.ivashko.99@gmail.com>
2024-11-07 21:31:03 +0300

    Создание сериализаторов

---
+++ BookStore/book_store/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from book_store.models import Author, Book
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'count', 'author']
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['id', 'first_name', 'last_name', 'books']
